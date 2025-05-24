5298edf0d1655d3eb35d2e5df5779436a034228c
dais <98775120+favolia@users.noreply.github.com>
2025-05-25 02:27:09 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Admin
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '08123456789',
+            'address' => 'Makassar',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+        ]);
+
+        // Budi
+        User::factory()->create([
+            'name' => 'budi',
+            'email' => 'budi@gmail.com',
+            'phone' => '08123456789',
+            'address' => 'Sudiang',
+            'password' => Hash::make('budi12345'),
+            'role' => 'user',
         ]);
     }
 }
